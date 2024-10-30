bc897aae3046d9c5cb9c270442901664154538f8
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-10-30 22:52:32 +0300

    Add bignumber to be used for counter value as files may exceed unsigned long long type

---
+++ bignumber.cpp
@@ -0,0 +1,56 @@
+#include "bignumber.h"
+
+BigNumber BigNumber::operator+(const BigNumber& other) const {
+    std::string result;
+    int i = num_.length() - 1, j = other.num_.length() - 1, carry = 0;
+
+    while (i >= 0 || j >= 0 || carry) {
+        int digit1 = i >= 0 ? num_[i] - '0' : 0;
+        int digit2 = j >= 0 ? other.num_[j] - '0' : 0;
+        int sum = digit1 + digit2 + carry;
+        carry = sum / 10;
+        result = std::to_string(sum % 10) + result;
+        i--;
+        j--;
+    }
+
+    return BigNumber(result);
+}
+
+BigNumber BigNumber::operator-(const BigNumber& other) const {
+    std::string result;
+    int i = num_.length() - 1, j = other.num_.length() - 1, borrow = 0;
+
+    while (i >= 0 || j >= 0 || borrow) {
+        int digit1 = i >= 0 ? num_[i] - '0' : 0;
+        int digit2 = j >= 0 ? other.num_[j] - '0' : 0;
+        int diff = digit1 - digit2 - borrow;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        }
+        else {
+            borrow = 0;
+        }
+        result = std::to_string(diff) + result;
+        i--;
+        j--;
+    }
+
+    // Remove leading zeros  
+
+    while (result.length() > 1 && result[0] == '0') {
+        result.erase(0, 1);
+    }
+
+    return BigNumber(result);
+}
+
+bool BigNumber::operator==(const BigNumber& other) const {
+    return num_ == other.num_;
+}
+
+std::ostream& operator<<(std::ostream& os, const BigNumber& num)
+{
+    return os << num.num_;
+}
---
+++ bignumber.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class BigNumber {
+public:
+    BigNumber(const std::string& num = "0");
+
+    BigNumber operator+(const BigNumber& other) const;
+    BigNumber operator-(const BigNumber& other) const;
+    bool operator==(const BigNumber& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const BigNumber& num);
+private:
+    std::string num_;
+};
