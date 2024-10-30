e77b5d596edd65001eb6809820888fba39be550a
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-10-30 22:52:01 +0300

    Parse tokens and read files chunk by chunk and count based on provided options

--- wc.cpp
+++ wc.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "wc.h"
+#include <fstream>
 
 int main(int argc, const char* argv[]) {
 	parser::Tokenizer tokenizer;
@@ -12,6 +13,39 @@ int main(int argc, const char* argv[]) {
 			if (i + 1 < argc) inProgram.append(" ");
 		}
 		std::vector<parser::Token> tokens = tokenizer.parse(inProgram);
+		parser::Parser parser;
+		parser.parse(tokens);
+		std::unordered_set<parser::Option> parsedOptions = parser.getOptions();
+
+		std::vector<Counter> counters;
+		if (!parsedOptions.empty()) {
+			for (auto& option : parsedOptions) {
+				Counter* counter = CounterFactory::createCounter(option);
+				counters.push_back(*counter);
+			}
+		}
+
+		std::vector<std::string> parsedFilenames = parser.getFilenames();
+		if (!parsedFilenames.empty()) {
+			for (auto& filename : parsedFilenames) {
+				std::ifstream file(filename);
+				constexpr size_t bufferSize = static_cast<size_t>(1024) * 1024;
+				std::unique_ptr<char[]> buffer(new char[bufferSize]);
+				while (file) {
+					file.read(buffer.get(), bufferSize);
+					std::string text(buffer.get());
+					for (auto& counter : counters) {
+						counter.count(text);
+					}
+				}
+			}
+		}
+
+		for (auto& counter : counters) {
+			std::cout << counter.getValue() << std::endl;
+		}
+
+
 	}
 	else {
 		std::cout << "No args have been passed" << std::endl;
--- wc.h
+++ wc.h
@@ -6,5 +6,8 @@
 #include <iostream>
 #include <iostream>
 #include "tokenizer.h"
+#include "parser.h"
+#include "counter.h"
+#include "counter-factory.h"
 #include <vector>
 // TODO: Reference additional headers your program requires here.
